1bd184dfcac0e0424c043a0f305e077abd8f3986
rmit-s3542846-Dominic-Alvaro <DomAlvaro@Dominics-MacBook-Pro-2.local>
2016-05-18 14:23:34 +1000

    Made a receipt.php page
    No css has been added as of yet, just outline of a Payment Receipt and
    below is the cinema ticket

---
+++ receipt.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Reciept Page</title>
+</head>
+<style>
+	table {
+    border: 1px solid black;
+}
+
+
+</style>
+<body>
+	<table>
+		<tr>
+			<td>Name:</td>
+			<td>Dom</td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td>dom@hotmail.com</td>
+			
+		</tr>
+		<tr>
+			<td>Movie:</td>
+			<td>Superman</td>
+		</tr>
+		<tr>
+			<td>Mobile Number:</td>
+			<td>04123456</td>
+		</tr>
+
+		<tr>
+			<td>Session Time:</td>
+			<td>9PM Monday</td>
+		</tr>
+
+		<tr>
+			<td>Ticket Type:</td>
+			<td>2 x Adult</td>
+		</tr>
+
+		<tr>
+			<td>Total:</td>
+			<td>$39.00</td>
+		</tr>
+	</table>
+
+	<table>
+		<tr>
+			<td>Silverado</td>
+		</tr>
+		<tr>
+			<td>Session Time:</td>
+			<td>9pm Monday</td>
+		</tr>
+		<tr>
+			<td>Movie:</td>
+			<td>Superman</td>
+		</tr>
+		<tr>
+			<td>Ticket:</td>
+			<td>Standard Adult</td>
+		</tr>
+	</table>
+	
+</body>
+</html>
